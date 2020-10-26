eded6e04c608fd076bd4396a9f03f4c575d64842
Alireza Kazemi <a.kazemi.a2001@gmail.com>
2020-10-26 16:00:27 +0330

    Add HWS4

---
+++ HWS 4/src/Main.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        
+        Scanner input = new Scanner(System.in);
+        System.out.println("1) library information");
+        System.out.println("2) National card registration");
+        System.out.println("3) Get a book");
+        System.out.print("\nEnter your select : ");
+        int a = input.nextInt();
+        
+        
+        
+        
+    }
+    
+}
---
+++ HWS 4/src/Rules.java
@@ -0,0 +1,57 @@
+import java.util.*;
+
+public class Rules {
+    String[][] book = new String[1][10];
+    int[][] book_code = new int[1][10];
+    
+    public Rules(){
+        this.book[0][0] = "the power of habit (11)";
+        this.book_code[0][0] = 11;
+        this.book[0][1] = "the compound effect (22)";
+        this.book_code[0][1] = 22;
+        this.book[0][2] = "Harry Potter (33)";
+        this.book_code[0][2] = 33;
+        this.book[0][3] = "Think and Grow Rich (44)";
+        this.book_code[0][3] = 44;
+        this.book[0][4] = "The Lord Of The Rings (55)";
+        this.book_code[0][4] = 55;
+        this.book[0][5] = "Le petit prince (66)";
+        this.book_code[0][5] = 66;
+        this.book[0][6] = "O Alquimista (77)";
+        this.book_code[0][6] = 77;
+        this.book[0][7] = "Il Nome della Rosa (88)";
+        this.book_code[0][7] = 88;
+        this.book[0][8] = "The Stranger (99)";
+        this.book_code[0][8] = 99;
+        this.book[0][9] = "The Wright Brothers (101)";
+        this.book_code[0][9] = 101;
+    }
+    
+    
+        
+    public void lending_book(){
+        
+    }
+    
+    
+    
+    public void receive_card(){
+        System.out.println("\nYour national card has been successfully registered");
+    }
+    
+    
+    
+    public void inscribe_information(){
+        final String file_number = "12345";
+        final String city = "tehran";
+        final String phone_number = "01010101";
+        final String zip_code  = "221133";
+        final String library_area = "1000";
+        final String numbers_of_books = "10";
+        final String numbers_of_chairs = "50";
+        final String numbers_of_employees = "5";
+        
+    }
+    
+ 
+}
